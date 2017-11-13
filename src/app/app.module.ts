d37637e1bfb104a7c6fbc47d9aaaeae618449344
jasonwang911 <396689144@qq.com>
2017-11-13 17:31:20 +0800

    ng4路由，官网

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { ErrPageComponent } from './err-page/err-page.component';
+
+const appRoutes: Routes =  [
+  {path: 'home', component: HomeComponent},
+  {path: 'nav', component: NavComponent},
+  {path: 'product', component: ProductComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: ErrPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    ProductComponent,
+    ErrPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
